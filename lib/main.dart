7123c5990bdf0fb9665893e4e2c54a6298faeb5d
LionnelT <lionneltsuro56@gmail.com>
2023-07-25 16:15:01 +0200

    Refactored main.dart to adhere to SOLID Principles. Initially ,the Main class had two responsibilities: to create the MaterialApp widget and to provide the app's theme

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(const Main());
 }
+
 /// This is the entry point of the Flutter app.
 /// The [Main] widget serves as the root of the widget tree.
 /// It's the starting point of the app's UI hierarchy.
@@ -16,26 +17,35 @@ class Main extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Color Changing App',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
+      theme: ThemeProvider().getTheme(),
       home: const SplashScreen(),
+    );
+  }
+}
+
+/// This is a helper class that provides the app's theme.
+class ThemeProvider {
+  /// This method returns a ThemeData object that defines the app's color scheme
+  /// and other visual properties.
+  ThemeData getTheme() {
+    return ThemeData(
+      // This is the theme of your application.
+      //
+      // TRY THIS: Try running your application with "flutter run". You'll see
+      // the application has a blue toolbar. Then, without quitting the app,
+      // try changing the seedColor in the colorScheme below to Colors.green
+      // and then invoke "hot reload" (save your changes or press the "hot
+      // reload" button in a Flutter-supported IDE, or press "r" if you used
+      // the command line to start the app).
+      //
+      // Notice that the counter didn't reset back to zero; the application
+      // state is not lost during the reload. To reset the state, use hot
+      // restart instead.
+      //
+      // This works for code too, not just values: Most code changes can be
+      // tested with just a hot reload.
+      colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      useMaterial3: true,
     );
   }
 }
