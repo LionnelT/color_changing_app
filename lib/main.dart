2d464e11c3207e6724c909aa18d46eee7f8a3c68
LionnelT <lionneltsuro56@gmail.com>
2023-07-25 16:28:39 +0200

    Sorted pub dependencies and removed debugmode banner

--- lib/main.dart
+++ lib/main.dart
@@ -16,6 +16,7 @@ class Main extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Color Changing App',
       theme: ThemeProvider().getTheme(),
       home: const SplashScreen(),
